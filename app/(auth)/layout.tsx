88d42ed5682cdedf53626e7d47a95d7e06f678a9
Leonardo Parisi <parisii.leonardo>
2023-09-14 11:06:51 +0200

    final commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+import '../globals.css'
+import { ClerkProvider } from "@clerk/nextjs";
+
+export const metadata = {
+  title: 'Auth',
+  description: 'Discover the best cars in the world',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang='en'>
+        <body className='relative'>
+          <div className="w-full flex justify-center items-center min-h-screen">
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,8 +2,6 @@ import { SignUp } from "@clerk/nextjs";
  
 export default function Page() {
   return(
-    <div className="w-full h-full flex items-center justify-center">
       <SignUp />
-    </div>
   )
 }
